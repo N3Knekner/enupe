e1187f54ce2f264f9427183c1001fbb56d88c47e
N3Knekner <n3kcontato@gmail.com>
2020-09-20 20:22:50 -0300

    Upload do Servidor Inicial
    Servidor simples que apenas gerengia o frontend

---
+++ build/precache-manifest.4971e0d3a938f2fe71086b389d8a4360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb4075b3ca79de06c32fdbb1f92c386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aec02d01bf808b789eab",
+    "url": "/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "6e80ddb0633d4f2f3fc0",
+    "url": "/static/css/main.1141b10a.chunk.css"
+  },
+  {
+    "revision": "aec02d01bf808b789eab",
+    "url": "/static/js/2.709a7041.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.709a7041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e80ddb0633d4f2f3fc0",
+    "url": "/static/js/main.b0e06223.chunk.js"
+  },
+  {
+    "revision": "e5ebad93f4def19e9204",
+    "url": "/static/js/runtime-main.1fc713b4.js"
+  }
+]);
